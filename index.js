e5431f935bca9049ef40cdce2e4ff9afd8c141b0
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-07 22:48:09 +0700

    JS Testting | menulis kode pengujian

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ test('ddua tambah dua adalah empat', () => {
     expect(2+2).toBe(4);
 });
 
+describe('pengujian oleh aritmatika', () => {
+    test('#1 dua tambah dua adalah empat', () => {
+        expect(2+2).tobe(4);
+    });
+
+    test('#2 dua kali tiga adalah enam', () => {
+        expect(2+3).tobe(6);
+    });
+
+})
+
