0f2ae347b23d811a41cee85d5db4ecb6aff8071d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-07 22:45:04 +0700

    JS Testting | menulis kode pengujian

---
+++ index.js
@@ -0,0 +1,9 @@
+// menulis kode pengujian
+// test('deskripsi dari testcase kamu', () => {
+//     expect(perintah).matcher(nilai yang diekspektasikan);
+// });
+
+test('ddua tambah dua adalah empat', () => {
+    expect(2+2).toBe(4);
+});
+
