06510bcc33c3fac46037838260f8f8196c3c21e9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-08 15:42:51 +0700

    JS Testting | pengujian kode

--- gradeCalculation.js
+++ gradeCalculation.js
@@ -3,4 +3,19 @@ const averageExams = (valuesExam) => {
     if (!numberValidation) throw Error('please input number');
 
     const sumValues = valuesExam.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-}
+    return sumValues / valuesExam.length;
+};
+
+const isStudentPassExam = (valuesExam, name) => {
+    const minValues = 75;
+    const average = averageExams(valuesExam);
+
+    if (average > minValues) {
+        console.log(`${name} pass the exams`);
+    } else {
+        console.log(`${name} fail the exams`);
+        return false;
+    }
+};
+
+module.exports = { averageExams, isStudentPassExam };
